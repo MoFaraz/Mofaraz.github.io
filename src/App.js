d4971b6718e2de6a8ddf095c2a81320fc9d18e31
mofaraz <mopharaz@gmail.com>
2023-07-09 12:26:49 +0330

    create and add preloader

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import NavBar from "./Component/Navbar";
+import Pre from "../src/Component/Pre"
 import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
 import './style.css'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import {useEffect, useState} from "react";
 
 function App() {
+    const [load, updateLoad] = useState(true)
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            updateLoad(false)
+        }, 1200)
+
+        return () => clearTimeout(timer)
+    }, []);
   return (
       <BrowserRouter>
+          <Pre load={load}/>
             <NavBar />
       </BrowserRouter>
         );
