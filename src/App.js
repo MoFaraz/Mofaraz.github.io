fc1017040bf2863b9d6ebdc71dbb2e67345de378
mofaraz <mopharaz@gmail.com>
2023-07-09 14:53:40 +0330

    add Scroll and Home to the Project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import NavBar from "./Component/Navbar";
-import Pre from "../src/Component/Pre"
+import Preloader from "../src/Component/Pre";
+import ScrollToTop from "./Component/ScrollToTop";
 import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
 import './style.css'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {useEffect, useState} from "react";
+import Home from "./Component/Home/Home";
 
 function App() {
     const [load, updateLoad] = useState(true)
@@ -18,8 +20,14 @@ function App() {
     }, []);
   return (
       <BrowserRouter>
-          <Pre load={load}/>
-            <NavBar />
+          <div className='App' id={load ? 'no-scroll' : 'scroll'}>
+          <Preloader load={load}/>
+          <NavBar />
+          <ScrollToTop/>
+          <Routes>
+              <Route path='/' element={<Home />}/>
+          </Routes>
+          </div>
       </BrowserRouter>
         );
 }
